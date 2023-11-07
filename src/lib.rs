432c7c9dcb206a6dd32cbfbb6d0ef87203ab7c15
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-07 08:42:55 -0300

    1699357375/magical-chickens

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+use std::process::Command;
+use std::str;
+
+pub struct Version {
+    major: u32,
+    minor: u32,
+    release: u32,
+}
+
+impl Version {
+    pub fn new(major: u32, minor: u32, release: u32) -> Version {
+        Version {
+            major,
+            minor,
+            release,
+        }
+    }
+
+    pub fn increment_release(&mut self) {
+        if self.release == 99 {
+            self.major = 0;
+            self.increment_minor();
+            self.release = 0;
+        } else {
+            self.major = 0;
+            // self.minor = 0;
+            self.release += 1;
+        }
+    }
+
+    pub fn increment_minor(&mut self) {
+        if self.minor == 99 {
+            self.increment_major();
+            self.minor = 0;
+            self.release = 0;
+        } else {
+            // self.major = 0;
+            self.minor += 1;
+            // self.release = 0;
+        }
+    }
+
+    pub fn increment_major(&mut self) {
+        self.major += 1;
+        // self.minor = 0;
+        // self.release = 0;
+    }
+
+    pub fn format(&self) -> String {
+        format!("v{}.{}.{}", self.major, self.minor, self.release)
+    }
+}
+
+pub fn get_current_git_tag() -> Result<String, String> {
+    let output = Command::new("git")
+        .arg("describe")
+        .arg("--abbrev=0")
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let git_describe = str::from_utf8(&output.stdout).expect("Failed to convert to string");
+        Ok(git_describe.trim().to_string())
+    } else {
+        Err("Git describe command failed".to_string())
+    }
+}
+
+pub fn new_tag(new_tag: &str) -> Result<String, String> {
+    let output = Command::new("git")
+        .arg("tag")
+        .arg("-a")
+        .arg(new_tag)
+        .arg("-m")
+        .arg("new release")
+        .arg("main")
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let git_describe = str::from_utf8(&output.stdout).expect("Failed to convert to string");
+        Ok(git_describe.trim().to_string())
+    } else {
+        Err("Git describe command failed".to_string())
+    }
+}
+
+pub fn push_new_tag() -> Result<String, String> {
+    let output = Command::new("git")
+        .arg("push")
+        .arg("--tags")
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let git_describe = str::from_utf8(&output.stdout).expect("Failed to convert to string");
+        Ok(git_describe.trim().to_string())
+    } else {
+        Err("Git describe command failed".to_string())
+    }
+}
+
+pub fn show_version() -> String {
+    const NAME: &str = env!("CARGO_PKG_NAME");
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+    format!("{} via 🦀 v{}/2023", NAME, VERSION)
+}
